6202d99ba7c908076c77891f91ef3ebc98b84675
Zippi-wzp <truebatmandc@gmail.com>
2015-08-08 18:30:16 -0400

    Created server side file

---
+++ main.js
@@ -0,0 +1,13 @@
+var http = require("http");
+var server = http.createServer(function(request, response) {
+  console.log(request.url);
+  console.log(request.data);
+  var activity = {
+    activity: request.data.activity,
+    timeSpent: request.data.timeSpent
+  }
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write("<h1>Hello!</h1><p>You asked for <code>" + request.url + "</code></p>");
+  response.end();
+});
+server.listen(8000);
