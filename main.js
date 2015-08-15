d1da67aae01d5e5c287812ab149e917e63fe5f1f
Zippi-wzp <truebatmandc@gmail.com>
2015-08-14 21:08:09 -0400

    Added post request handling

--- main.js
+++ main.js
@@ -1,13 +1,34 @@
 var http = require("http");
 var server = http.createServer(function(request, response) {
-  console.log(request.url);
-  console.log(request.data);
-  var activity = {
-    activity: request.data.activity,
-    timeSpent: request.data.timeSpent
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.setHeader("Content-Type", "application/json");
+  var responseData;
+
+  switch (request.method) {
+    case "POST":
+      var jsonString = "";
+      request.on('data', function (data) {
+        jsonString += data;
+      });
+      request.on('end', function () {
+        var activity = JSON.parse(jsonString);
+        responseData = JSON.stringify(activity);
+        console.log(responseData);
+        response.statusCode = 201;
+        response.end(responseData);
+      });
+      break;
+    case "GET":
+      // response.data = [
+      //   { description: "lol", timeSpent: 13 },
+      //   { description: "hahaha", timeSpent: 4 },
+      //   { description: ":D", timeSpent: 3 }
+      // ];
+      // response.write("<h1>Hello!</h1><p>You asked for <code>" + request.url + "</code></p>");
+      // response.statusCode = 200;
+      // response.end(responseData);
+      break;
+    default: break;
   }
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("<h1>Hello!</h1><p>You asked for <code>" + request.url + "</code></p>");
-  response.end();
 });
 server.listen(8000);
