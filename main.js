937afb9e642729c2454dd5a616a63cc0da4ecb4c
Zippi-wzp <truebatmandc@gmail.com>
2015-08-15 18:42:10 -0400

    Added GET handler

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 var http = require("http");
+
+var activities = [];
+
+for (var i=1; i<=10; i++) {
+  activities.push({
+    description: "something",
+    timeSpent: i
+  });
+}
+
 var server = http.createServer(function(request, response) {
   response.setHeader("Access-Control-Allow-Origin", "*");
   response.setHeader("Content-Type", "application/json");
@@ -13,20 +23,16 @@ var server = http.createServer(function(request, response) {
       request.on('end', function () {
         var activity = JSON.parse(jsonString);
         responseData = JSON.stringify(activity);
-        console.log(responseData);
+        console.log("Going to send this JSON: " + responseData);
         response.statusCode = 201;
         response.end(responseData);
       });
       break;
     case "GET":
-      // response.data = [
-      //   { description: "lol", timeSpent: 13 },
-      //   { description: "hahaha", timeSpent: 4 },
-      //   { description: ":D", timeSpent: 3 }
-      // ];
-      // response.write("<h1>Hello!</h1><p>You asked for <code>" + request.url + "</code></p>");
-      // response.statusCode = 200;
-      // response.end(responseData);
+      responseData = JSON.stringify({activities: activities});
+      console.log("Going to send this JSON: " + responseData);
+      response.statusCode = 200;
+      response.end(responseData);
       break;
     default: break;
   }
