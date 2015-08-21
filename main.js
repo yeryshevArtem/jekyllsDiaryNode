4601b23ff2d855f8b6f712b5a9b93737ad5fa545
Zippi-wzp <truebatmandc@gmail.com>
2015-08-20 20:53:49 -0400

    Added mysql connection and fixed get and post requests

--- main.js
+++ main.js
@@ -1,40 +1,85 @@
 var http = require("http");
+var mysql = require("mysql");
 
-var activities = [];
-
-for (var i=1; i<=10; i++) {
-  activities.push({
-    description: "something",
-    timeSpent: i
-  });
-}
-
-var server = http.createServer(function(request, response) {
-  response.setHeader("Access-Control-Allow-Origin", "*");
-  response.setHeader("Content-Type", "application/json");
-  var responseData;
-
-  switch (request.method) {
-    case "POST":
-      var jsonString = "";
-      request.on('data', function (data) {
-        jsonString += data;
-      });
-      request.on('end', function () {
-        var activity = JSON.parse(jsonString);
-        responseData = JSON.stringify(activity);
-        console.log("Going to send this JSON: " + responseData);
-        response.statusCode = 201;
-        response.end(responseData);
-      });
-      break;
-    case "GET":
-      responseData = JSON.stringify({activities: activities});
-      console.log("Going to send this JSON: " + responseData);
-      response.statusCode = 200;
-      response.end(responseData);
-      break;
-    default: break;
-  }
+var connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'root',
+    port: '3306',
+    database: 'diary'
 });
-server.listen(8000);
+
+connection.connect(function (err) {
+    if (!err) {
+        var server = http.createServer(function (request, response) {
+            response.setHeader("Access-Control-Allow-Origin", "*");
+            response.setHeader("Content-Type", "application/json");
+            var responseData;
+
+            switch (request.method) {
+                case "POST":
+                    var jsonString = "";
+                    request.on('data', function (data) {
+                        jsonString += data;
+                    });
+                    request.on('end', function () {
+                        var activity = JSON.parse(jsonString);
+                        if (activity.method && activity.method === "DELETE") {
+                            responseData = "";
+                            response.statusCode = 204;
+                        }
+                        else {
+                            if (activity.activity.description && activity.activity.hours) {
+                                responseData = JSON.stringify(activity);
+                                var data = {description: activity.activity.description, hours: activity.activity.hours};
+                                var query = connection.query('INSERT INTO activities SET ?', data, function (err, result) {
+                                    if (!err) {
+                                        responseData = activity;
+                                        response.statusCode = 201;
+                                    }
+                                });
+                            } else {
+                                responseData = JSON.stringify({error: "Bad request motherfucker"});
+                                response.statusCode = 400;
+                            }
+                        }
+                        response.end(responseData);
+                    });
+                    break;
+                case "GET":
+                    var query = connection.query('SELECT * FROM activities', null, function (err, result) {
+                        responseData = JSON.stringify({activities: result});
+                        response.statusCode = 200;
+                        console.log(responseData);
+                        response.end(responseData);
+                    });
+                    break;
+                case "PUT":
+                    var jsonString = "";
+                    request.on('data', function (data) {
+                        jsonString += data;
+                    });
+                    request.on('end', function () {
+                        var activity = JSON.parse(jsonString);
+                        responseData = JSON.stringify(activity);
+                        response.statusCode = 201;
+                        response.end(responseData);
+                    });
+                    break;
+                case "DELETE":
+                    var jsonString = "";
+                    request.on('data', function (data) {
+                        jsonString += data;
+                    });
+                    request.on('end', function () {
+                        response.statusCode = 204;
+                        response.end(jsonString);
+                    });
+                    break;
+                default:
+                    break;
+            }
+        });
+        server.listen(8000);
+    }
+});
