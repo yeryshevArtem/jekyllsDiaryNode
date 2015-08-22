ba0a9ae394e59880b119708377725dd746b28518
Zippi-wzp <truebatmandc@gmail.com>
2015-08-22 16:53:21 -0400

    Finished PUT and DELETE

--- main.js
+++ main.js
@@ -13,6 +13,8 @@ connection.connect(function (err) {
     if (!err) {
         var server = http.createServer(function (request, response) {
             response.setHeader("Access-Control-Allow-Origin", "*");
+            response.setHeader("Allow", "*");
+            response.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
             response.setHeader("Content-Type", "application/json");
             var responseData;
 
@@ -24,25 +26,21 @@ connection.connect(function (err) {
                     });
                     request.on('end', function () {
                         var activity = JSON.parse(jsonString);
-                        if (activity.method && activity.method === "DELETE") {
-                            responseData = "";
-                            response.statusCode = 204;
+                        if (activity.activity.description && activity.activity.hours) {
+                            var data = {description: activity.activity.description, hours: activity.activity.hours};
+                            var query = connection.query('INSERT INTO activities SET ?', data, function (err, result) {
+                                if (!err) {
+                                    activity.activity["id"] = result.insertId;
+                                    responseData = JSON.stringify(activity);
+                                    responseData = activity;
+                                    response.statusCode = 201;
+                                }
+                            });
+                        } else {
+                            responseData = JSON.stringify({error: "Bad request motherfucker"});
+                            response.statusCode = 400;
                         }
-                        else {
-                            if (activity.activity.description && activity.activity.hours) {
-                                responseData = JSON.stringify(activity);
-                                var data = {description: activity.activity.description, hours: activity.activity.hours};
-                                var query = connection.query('INSERT INTO activities SET ?', data, function (err, result) {
-                                    if (!err) {
-                                        responseData = activity;
-                                        response.statusCode = 201;
-                                    }
-                                });
-                            } else {
-                                responseData = JSON.stringify({error: "Bad request motherfucker"});
-                                response.statusCode = 400;
-                            }
-                        }
+
                         response.end(responseData);
                     });
                     break;
@@ -50,7 +48,7 @@ connection.connect(function (err) {
                     var query = connection.query('SELECT * FROM activities', null, function (err, result) {
                         responseData = JSON.stringify({activities: result});
                         response.statusCode = 200;
-                        console.log(responseData);
+
                         response.end(responseData);
                     });
                     break;
@@ -61,9 +59,17 @@ connection.connect(function (err) {
                     });
                     request.on('end', function () {
                         var activity = JSON.parse(jsonString);
-                        responseData = JSON.stringify(activity);
-                        response.statusCode = 201;
-                        response.end(responseData);
+                        if (activity.activity.description && activity.activity.hours) {
+                            var data = [activity.activity.description, activity.activity.hours, activity.activity.id ];
+                            var query = connection.query('UPDATE activities SET description=?, hours=? WHERE id=?', data, function (err, result) {
+                                if (!err) {
+                                    response.statusCode = 204;
+                                }
+                            });
+                        } else {
+                            response.statusCode = 400;
+                        }
+                        response.end();
                     });
                     break;
                 case "DELETE":
@@ -72,9 +78,25 @@ connection.connect(function (err) {
                         jsonString += data;
                     });
                     request.on('end', function () {
-                        response.statusCode = 204;
-                        response.end(jsonString);
+                        var activity = JSON.parse(jsonString);
+                        if (activity.activity.id) {
+                            var data = [activity.activity.id];
+                            var query = connection.query('DELETE FROM activities WHERE id=?', data, function (err, result) {
+                                if (!err) {
+                                    response.statusCode = 204;
+                                }
+                            });
+                        } else {
+                            response.statusCode = 400;
+                        }
+                        response.end();
                     });
+                    break;
+                case "OPTIONS":
+                    response.statusCode = 200;
+                    response.setHeader("Allow", "*");
+                    response.end();
+
                     break;
                 default:
                     break;
